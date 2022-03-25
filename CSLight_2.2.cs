b4d0d682bf08eadd24af0fec833a1269d3f43eb6
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-26 01:16:13 +0700

    Create CSLight_2.2.cs

---
+++ CSLight_2.2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSLight_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWorking = true;
+            string keyword;
+
+            while (isWorking) 
+            {
+                keyword = Console.ReadLine();
+                
+                if (keyword == "exit")
+                {
+                    isWorking = false;
+                }
+            }
+        }
+    }
+}
