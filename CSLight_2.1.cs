1d149fe38c6fd5551b23d725862e0ae266da8980
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-26 00:59:13 +0700

    Create CSLight_2.1.cs

---
+++ CSLight_2.1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSLight_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userPhrase;
+            int iterationCount;
+
+            Console.WriteLine("Введите фразу, которую хотите зациклить: ");
+            userPhrase = Console.ReadLine();
+            Console.Write("Введите желаемое количество повторов: ");
+            iterationCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < iterationCount; i++)
+            {
+                Console.WriteLine(userPhrase);
+            }
+        }
+    }
+}
