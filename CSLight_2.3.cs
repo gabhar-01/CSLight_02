bd89407e5bb4a2cead0f011101f8f4bb40e43e9d
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-26 01:37:11 +0700

    Create CSLight_2.3.cs

---
+++ CSLight_2.3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSLight_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i <= 96; i += 7) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
