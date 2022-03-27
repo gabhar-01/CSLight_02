d9042de138f32729d67f776e6127f2adb3eb4158
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-27 15:18:17 +0700

    Create CSLight_2.8.cs

---
+++ CSLight_2.8.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSLight_2._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "Cat";
+            string userInput;
+            int numberOfTries = 3;
+
+            while (numberOfTries !=0 )
+            {
+                Console.Write("Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    numberOfTries = 0;
+                    Console.WriteLine("Ночью все кошки серы.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    numberOfTries--;
+                    Console.WriteLine("Пароль неверный. У вас осталось " + numberOfTries + " попытки.");
+                }
+            }
+        }
+    }
+}
