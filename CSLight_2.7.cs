b3cb262841d2ca4c7997efe7d3a0aefbd98dad61
gabhar-01 <shevchenko.lilya@mail.ru>
2022-03-27 14:47:45 +0700

    Create CSLight_2.7.cs

---
+++ CSLight_2.7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSLight_2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            char userSymbol;
+
+            Console.Write("Введите имя: ");
+            userName = Console.ReadLine();
+            Console.Write("Введите символ: ");
+            userSymbol = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine();
+
+            int symbolsCount = userName.Length + 2;
+
+            for (int i = 0; i < symbolsCount; i++)
+            {
+                Console.Write(userSymbol);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(userSymbol + userName + userSymbol);
+
+            for (int i = 0; i < symbolsCount; i++)
+            {
+                Console.Write(userSymbol);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
