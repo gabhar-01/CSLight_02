fbaecc178fe2f19c423bd832eb826036b25b32de
gabhar-01 <shevchenko.lilya@mail.ru>
2022-04-01 15:33:27 +0700

    Create CSLight_2.11.cs

---
+++ CSLight_2.11.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CSLight_2._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bracketedText;
+            bool isBracketedTextCorrect = true;
+
+            int maxAmountOfNestedBrackets = 0;
+            int currentAmountOfNestedBrackets = 0;
+            int minBorder = 0;
+
+            Console.Write("Введите скобочное выражение: ");
+            bracketedText = Console.ReadLine();
+
+            foreach (char bracket in bracketedText)
+            {
+                if (bracket == '(')
+                {
+                    currentAmountOfNestedBrackets++;
+
+                    if (maxAmountOfNestedBrackets < currentAmountOfNestedBrackets)
+                    {
+                        maxAmountOfNestedBrackets = currentAmountOfNestedBrackets;
+                    }
+                }
+                else if (bracket == ')')
+                {
+                    currentAmountOfNestedBrackets--;
+
+                    if (currentAmountOfNestedBrackets < minBorder)
+                    {
+                        isBracketedTextCorrect = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (currentAmountOfNestedBrackets != minBorder || maxAmountOfNestedBrackets == minBorder)
+            {
+                isBracketedTextCorrect = false;
+            }
+
+            if (isBracketedTextCorrect == true)
+            {
+                Console.WriteLine("Строка является корректным скобочным выражением. Максимальная глубина вложенности скобок равна " +
+                    maxAmountOfNestedBrackets + ".");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является корректным скобочным выражением.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
